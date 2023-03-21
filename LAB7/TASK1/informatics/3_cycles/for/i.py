8037b0012fb318964f75fde403bc9bea129a9fd7
Darina <dajtbaeva@gmail.com>
2023-03-21 18:30:19 +0600

    upd

--- LAB7/TASK1/informatics/3_cycles/for/i.py
+++ LAB7/TASK1/informatics/3_cycles/for/i.py
@@ -1,14 +1,15 @@
 '''
 Задача №341. Количество делителей
-Выведите единственное число - количество делителей числа x.
+Выведите единственное число - количество делителей числа a.
 '''
 from math import sqrt
-a, cnt = int(input()), 0
-# for i in range(1, int(sqrt(a)) + 1):
-#     if a % i == 0:
-#         cnt += 2
-cnt = 2 # 1 и само число
-for i in range(2, int(a/2)+1): 
+
+a, res = int(input()), 0
+
+for i in range(1, int(sqrt(a)) + 1):
     if a % i == 0:
-        cnt += 1
-print(cnt)
+        res += 1
+        if i != a // i:
+            res += 1
+
+print(res)
